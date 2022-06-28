d6773b4b980130f1d2341d6be67830bc7f089188
bwmann <52176989+bwmann@users.noreply.github.com>
2022-06-28 18:29:49 -0400

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -4,9 +4,9 @@ include_controls 'rds-bp-benchmark' do
 
   control "tags" do
     impact 0.0
-    desc 'caveat', 'This is Not Applicable since our ACME Team is not using Terraform to create RDS instances'
-    describe 'This is Not Applicable since our ACME Team is not using Terraform to create RDS instances.' do
-      skip 'This is Not Applicable since our ACME Team is not using Terraform to create RDS instances.'
+    desc 'caveat', 'This is Not Applicable since IDRS is using Cloud Formation instead of Terraform to create RDS instances'
+    describe 'This is Not Applicable since IDRS is using Cloud Formation instead of Terraform to create RDS instances.' do
+      skip 'This is Not Applicable since IDRS is using Cloud Formation instead of Terraform to create RDS instances.'
     end    
   end
 
